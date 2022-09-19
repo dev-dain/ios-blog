4e9b5396b9741ffb42319d185e95383047c69b3f
dev-dain <dev.dain.k@gmail.com>
2022-09-19 21:51:56 +0900

    ViewController 코드 정리

--- FSCalendarEx/FSCalendarEx/ViewController.swift
+++ FSCalendarEx/FSCalendarEx/ViewController.swift
@@ -8,23 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let label: UILabel = {
-        let label = UILabel()
-        label.text = "코드 베이스로 만든 뷰입니다"
-        label.sizeToFit()
-        label.font = .preferredFont(forTextStyle: .headline)
-        label.textColor = .label
-        return label
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray
-        view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true        
+        view.backgroundColor = .systemBackground
     }
 }
