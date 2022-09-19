e80d98fd82d919e016ebca4fc1037b10be491c61
dev-dain <dev.dain.k@gmail.com>
2022-09-19 22:40:48 +0900

    달력 예제 완성

--- FSCalendarEx/FSCalendarEx/ViewController.swift
+++ FSCalendarEx/FSCalendarEx/ViewController.swift
@@ -6,11 +6,54 @@
 //
 
 import UIKit
+import FSCalendar
+import SnapKit
 
 class ViewController: UIViewController {
+    private let calendar: FSCalendar = {
+        let calendar = FSCalendar(frame: .zero)
+        
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.backgroundColor = .systemBackground
+        calendar.scrollEnabled = true
+        calendar.scrollDirection = .horizontal
+        
+        calendar.appearance.headerDateFormat = "YYYY. M"
+        calendar.appearance.headerTitleColor = .label
+        calendar.appearance.headerTitleAlignment = .center
+        calendar.headerHeight = 40.0
+        calendar.placeholderType = .none
+        
+        return calendar
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        view.addSubview(calendar)
+        
+        calendar.delegate = self
+        
+        calendar.snp.makeConstraints {
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.width.equalToSuperview()
+        }
+    }
+}
+
+extension ViewController: FSCalendarDelegateAppearance {
+    // 토요일 파랑, 일요일 빨강으로 만들기
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        let day = Calendar.current.component(.weekday, from: date) - 1
+        
+        if Calendar.current.shortWeekdaySymbols[day] == "일" {
+            return .systemRed
+        } else if Calendar.current.shortWeekdaySymbols[day] == "토" {
+            return .systemBlue
+        } else {
+            return .label
+        }
     }
 }
