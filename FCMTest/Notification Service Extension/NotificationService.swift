cdbdaf1f682e5a54ac6264b302263efe163ebc31
dev-dain <dev.dain.k@gmail.com>
2022-12-18 14:32:09 +0900

    페이로드를 이용한 사진 푸시 예제

--- FCMTest/Notification Service Extension/NotificationService.swift
+++ FCMTest/Notification Service Extension/NotificationService.swift
@@ -17,8 +17,32 @@ class NotificationService: UNNotificationServiceExtension {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        guard let bestAttemptContent = bestAttemptContent else { return }
-        FIRMessagingExtensionHelper().populateNotificationContent(bestAttemptContent, withContentHandler: contentHandler)
+        if let bestAttemptContent = bestAttemptContent,
+           let fcmOptionsUserInfo = bestAttemptContent.userInfo["fcm_options"] as? [String: Any] {
+            // Modify the notification content here...
+            guard let urlImageString = fcmOptionsUserInfo["image"] as? String else {
+                contentHandler(bestAttemptContent)
+                return
+            }
+
+            if let imageUrl = URL(string: urlImageString) {
+                // 이미지 다운로드
+                guard let imageData = try? Data(contentsOf: imageUrl) else {
+                    contentHandler(bestAttemptContent)
+                    return
+                }
+
+                // UNNotificationAttachment 세팅
+                guard let attachment = UNNotificationAttachment.saveImageToDisk(identifier: "myImage.jpg", data: imageData, options: nil) else {
+                    contentHandler(bestAttemptContent)
+                    return
+                }
+
+                bestAttemptContent.attachments = [attachment]
+                Messaging.serviceExtension().populateNotificationContent(bestAttemptContent, withContentHandler: self.contentHandler!)
+                contentHandler(bestAttemptContent)
+            }
+        }
     }
     
     override func serviceExtensionTimeWillExpire() {
@@ -29,4 +53,23 @@ class NotificationService: UNNotificationServiceExtension {
         }
     }
 
+}
+
+extension UNNotificationAttachment {
+    static func saveImageToDisk(identifier: String, data: Data, options: [AnyHashable : Any]? = nil) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let folderName = ProcessInfo.processInfo.globallyUniqueString
+        let folderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(folderName, isDirectory: true)!
+
+        do {
+            try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
+            let fileURL = folderURL.appendingPathExtension(identifier)
+            try data.write(to: fileURL)
+            let attachment = try UNNotificationAttachment(identifier: identifier, url: fileURL, options: options)
+            return attachment
+        } catch {
+            print("saveImageToDisk error - \(error)")
+        }
+        return nil
+    }
 }
